357e7b5cb1a8733809b8d63fe4a64f24d520857d
ziom <lagiewnik@gmail.com>
2022-08-24 14:32:46 +0200

    add never type

--- dist/helpers/render-categories.helper.js
+++ dist/helpers/render-categories.helper.js
@@ -1,3 +1,21 @@
+const handleCategoryChange = (category) => {
+    if (category === 'general') {
+        console.log("Zmiana na general");
+    }
+    else if (category === "poker") {
+        alert("ALLIN");
+    }
+    else if (category === "work") {
+        document.body.style.background = "blue";
+    }
+    else if (category === "hobby") {
+        document.body.style.background = "green";
+    }
+    else {
+        console.log("nigdy sie nie wyswietli");
+        const never = category; //bedzie  błąd jeśli w typie category: Category zostały by nieobsłużone wyżej wartości czyli jeśli poza tymi 4 wyżej byłaby 5 kategoria
+    }
+};
 export const renderCategories = (categories, categoriesContainerElement, inputChangeCallback) => {
     categories.forEach(category => {
         const categoryElement = document.createElement("li");
@@ -9,6 +27,7 @@ export const renderCategories = (categories, categoriesContainerElement, inputCh
         radioInputElement.addEventListener("change", () => {
             inputChangeCallback(category);
             //console.log("afterr radiobutton click: " + category + " selected category: " + selectedCategory)
+            handleCategoryChange(category);
         });
         const labelElement = document.createElement("label");
         labelElement.setAttribute("for", `category-${category}`);
--- src/helpers/render-categories.helper.ts
+++ src/helpers/render-categories.helper.ts
@@ -1,6 +1,20 @@
 import { Category } from "../types/types"
 
-
+const handleCategoryChange = (category: Category) => {
+    if (category === 'general') {
+        console.log("Zmiana na general");
+    } else if (category === "poker"){
+        alert ("ALLIN")
+    } else if (category === "work") {
+        document.body.style.background = "blue"
+    } else if (category === "hobby") {
+        document.body.style.background = "green"
+    }
+    else {
+        console.log("nigdy sie nie wyswietli")
+        const never: never = category; //bedzie  błąd jeśli w typie category: Category zostały by nieobsłużone wyżej wartości czyli jeśli poza tymi 4 wyżej byłaby 5 kategoria
+    }
+}
 export const renderCategories = (categories: Category[], categoriesContainerElement: HTMLElement, inputChangeCallback: (category: Category) => void) => {
 
     categories.forEach(category => {
@@ -13,7 +27,7 @@ export const renderCategories = (categories: Category[], categoriesContainerElem
         radioInputElement.addEventListener("change", ()=>{
             inputChangeCallback(category)
             //console.log("afterr radiobutton click: " + category + " selected category: " + selectedCategory)
-
+            handleCategoryChange(category)
         })
 
         const labelElement: HTMLLabelElement = document.createElement("label")
